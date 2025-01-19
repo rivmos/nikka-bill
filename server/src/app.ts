ba2e74bfde33bd00972efc063580c398ec110386
rivmos <rivmos.official@gmail.com>
2025-01-19 21:51:08 +0530

    initial mongo setup done

---
+++ server/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import mongoose from 'mongoose';
+import config from './utils/config';
+
+const app = express()
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(config.MONGO_URI as string).then(() => {
+    console.log('mongo kissed')
+}).catch(() => {
+    console.log('mongo not kissed')
+})
+
+
+app.get('/', (req, res) => {
+    res.json({message: ''})
+})
+
+export default app
